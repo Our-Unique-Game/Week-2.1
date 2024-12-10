d0eac87c6779cf3a0cc3166039b888c3a130b9c2
Roi Bruchim <roibr23@gmail.com>
2024-12-10 19:14:05 +0200

    fix problems

--- Assets/Scripts/Rotator.cs
+++ Assets/Scripts/Rotator.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class Rotator : MonoBehaviour
 {
 
-
+    [SerializeField, Tooltip("Speed of rotation")]
     public Vector3 rotationSpeed = new Vector3(0, 100, 0);
 
     void Update()
