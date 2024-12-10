a24b4504cf579524d176affa02900b3395a43353
Roi Bruchim <roibr23@gmail.com>
2024-12-10 19:19:47 +0200

    final

--- Assets/Scripts/Rotator.cs
+++ Assets/Scripts/Rotator.cs
@@ -6,8 +6,7 @@ public class Rotator : MonoBehaviour
     [SerializeField, Tooltip("Speed of rotation")]
     public Vector3 rotationSpeed = new Vector3(0, 100, 0);
 
-    void Update()
-    {
+    void Update(){
         transform.Rotate(rotationSpeed * Time.deltaTime);
     }
 }
--- Assets/Scripts/mover.cs
+++ Assets/Scripts/mover.cs
@@ -7,14 +7,12 @@ public class mover : MonoBehaviour
     float speed = 1.0f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
+    void Start(){
         //Debug.Log("Starting");
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update(){
         // GetComponent<Transform>().Position += new Vector3(speed * Time.deltaTime, 0, 0);
         transform.position += new Vector3(speed * Time.deltaTime, 0, 0);
 
